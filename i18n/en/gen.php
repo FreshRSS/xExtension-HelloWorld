311ca613984e961c21a10c46727fa4a4901327f6
mathContao <math-home@web.de>
2023-03-22 21:49:55 +0100

    i18n en

--- i18n/en/gen.php
+++ i18n/en/gen.php
@@ -1,7 +1,20 @@
 <?php
 
+/******************************************************************************/
+/* Each entry of that file can be associated with a comment to indicate its   */
+/* state. When there is no comment, it means the entry is fully translated.   */
+/* The recognized comments are (comment matching is case-insensitive):        */
+/*   + TODO: the entry has never been translated.                             */
+/*   + DIRTY: the entry has been translated but needs to be updated.          */
+/*   + IGNORE: the entry does not need to be translated.                      */
+/* When a comment is not recognized, it is discarded.                         */
+/******************************************************************************/
+
 return array(
 	'action' => array(
-		'submit' => 'Save',
+		'submit' => 'OK Hello World!',
+	),
+	'menu' => array(
+		'about' => 'About FreshRSS and Hello World',
 	),
 );
