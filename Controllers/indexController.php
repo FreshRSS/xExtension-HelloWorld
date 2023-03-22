7c3948c2b082102c1e31ce71e2eaf163a339435f
mathContao <math-home@web.de>
2023-03-22 21:49:40 +0100

    fix title of about page

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -2,6 +2,6 @@
 
 class FreshExtension_index_Controller extends FreshRSS_index_Controller {
 	public function aboutAction() {
-		Minz_View::prependTitle(_t('ext.about.new_title') . ' · ');
+		Minz_View::prependTitle(_t('ext.hello_world.about.title') . ' · ');
 	}
 }
